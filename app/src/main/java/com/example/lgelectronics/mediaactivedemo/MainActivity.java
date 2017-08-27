c4072f078fdd302db0edc8c006eba589840ca421
enolynowr <devkimwr@gmail.com>
2017-08-27 22:35:11 +0900

    20170827(final05)

--- app/src/main/java/com/example/lgelectronics/mediaactivedemo/MainActivity.java
+++ app/src/main/java/com/example/lgelectronics/mediaactivedemo/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.lgelectronics.mediaactivedemo;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,12 +15,13 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
     public static String LOG_TAG = "MEDIA_ACTIVE_DEMO_LOG::";
     private String bn_ads = EnumBundle.AdmobKey.BANNER_ADS_KEY.getString(); //Banner Ads key
     //ListView Text
@@ -34,6 +38,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_PHONE_STATE);
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_PHONE_STATE}, 1);
+        } else {
+            //TODO
+        }
+
+
         //Detect Device ID
         telephony = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
         Log.d(LOG_TAG, "getDeviceId : " + telephony.getDeviceId());
@@ -58,6 +72,21 @@ public class MainActivity extends AppCompatActivity {
 
         AdRequest adRequest = new AdRequest.Builder().addTestDevice(telephony.getDeviceId()).build();
         mAdView.loadAd(adRequest);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    //TODO
+                    Toast.makeText(getApplicationContext(), "READ_PHONE_STATE-PERMISSION_GRANTED", Toast.LENGTH_LONG).show();
+                }
+                break;
+
+            default:
+                break;
+        }
     }
 
     //Setting Dialog
