720d9e8d7fc9885d15f10c021d850dab64f8b35c
enolynowr <devkimwr@gmail.com>
2017-08-27 23:39:19 +0900

    20170827(final06)

--- app/src/main/java/com/example/lgelectronics/mediaactivedemo/MainActivity.java
+++ app/src/main/java/com/example/lgelectronics/mediaactivedemo/MainActivity.java
@@ -38,20 +38,6 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        int permissionCheck = ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_PHONE_STATE);
-
-        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_PHONE_STATE}, 1);
-        } else {
-            //TODO
-        }
-
-
-        //Detect Device ID
-        telephony = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-        Log.d(LOG_TAG, "getDeviceId : " + telephony.getDeviceId());
-
         //Setting ListView
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, music);
         ListView listView = (ListView) findViewById(R.id.listViewMain);
@@ -70,32 +56,38 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         MobileAds.initialize(getApplicationContext(), bn_ads);
         mAdView = (AdView) findViewById(R.id.adView);
 
-        AdRequest adRequest = new AdRequest.Builder().addTestDevice(telephony.getDeviceId()).build();
-        mAdView.loadAd(adRequest);
-    }
+        int permissionCheck = ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_PHONE_STATE);
+        //Permission Check
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_PHONE_STATE}, 1);
+        } else {
+            loadAd();
+        }
 
-    @Override
+    }//onCreate
+
+    @Override//Permission Result
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
-            case 1:
+            case 1://Receive Permission Load Ads
                 if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    //TODO
-                    Toast.makeText(getApplicationContext(), "READ_PHONE_STATE-PERMISSION_GRANTED", Toast.LENGTH_LONG).show();
+                    loadAd();
+                } else {
+                    //Toast.makeText(getApplicationContext(), "PERMISSION_NOT_ALLOWED", Toast.LENGTH_LONG).show();
+                    showKillAppDialLog();
                 }
-                break;
-
+                break;//case 1
             default:
                 break;
         }
     }
 
-    //Setting Dialog
+    //Setting Dialog Of Playing
     private void showDialLog(final int _position) {
-
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
         alertDialogBuilder.setTitle(this.getResources().getString(R.string.main_dialog_title));
         alertDialogBuilder.setMessage(this.getResources().getString(R.string.main_dialog_message));
-        //Positive Button
+        //Positive Button Of Playing
         alertDialogBuilder.setPositiveButton(this.getResources().getString(R.string.dialog_positive), new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
                 Intent intent = new Intent(MainActivity.this, ConnectScreenActivity.class);
@@ -103,7 +95,7 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
                 startActivity(intent);
             }
         });
-        //Negative Button
+        //Negative Button Of Playing
         alertDialogBuilder.setNegativeButton(this.getResources().getString(R.string.dialog_negative), new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
                 dialog.cancel();
@@ -111,4 +103,36 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         });
         alertDialogBuilder.show();
     }
+    //Load Ads
+    private void loadAd() {
+        //Detect Device ID
+        telephony = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        Log.d(LOG_TAG, "getDeviceId : " + telephony.getDeviceId());
+
+        AdRequest adRequest = new AdRequest.Builder().addTestDevice(telephony.getDeviceId()).build();
+        mAdView.loadAd(adRequest);
+    }
+
+    //Setting Dialog Of Permission Diallog
+    private void showKillAppDialLog() {
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle(this.getResources().getString(R.string.permission_denied));
+        alertDialogBuilder.setMessage(this.getResources().getString(R.string.permission_denied_message));
+        //Positive Button
+        alertDialogBuilder.setPositiveButton(this.getResources().getString(R.string.dialog_positive), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                killApp();
+            }
+        });
+        alertDialogBuilder.setCancelable(false);
+        alertDialogBuilder.show();
+    }
+
+    private void killApp() {
+        moveTaskToBack(true);
+        finish();
+        android.os.Process.killProcess(android.os.Process.myPid());
+    }
+
 }
